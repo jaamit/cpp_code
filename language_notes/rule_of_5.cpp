c2f027d3aa13939d7312a2f19ec0b44f1c0f7550
Amit Jain <amit.hpepc@gmail.com>
2018-02-13 12:57:05 +0530

    Update rule_of_5.cpp

--- language_notes/rule_of_5.cpp
+++ language_notes/rule_of_5.cpp
@@ -1,7 +1,7 @@
 class C {
   C(const C&) = default;               // Copy constructor
   C(C&&) = default;                    // Move constructor
-  C& operator=(const C&) & = default;  // Copy assignment operator
-  C& operator=(C&&) & = default;       // Move assignment operator
+  C& operator=(const C&) = default;    // Copy assignment operator
+  C& operator=(C&&) = default;         // Move assignment operator
   virtual ~C() { }                     // Destructor
 };
