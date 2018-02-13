37152174880b12fa1033b773e9dc1436ea42a438
Amit Jain <amit.hpepc@gmail.com>
2018-02-13 16:19:59 +0530

    Create 2d_array.cpp

---
+++ language_notes/2d_array.cpp
@@ -0,0 +1,41 @@
+// Example program 2D array
+#include <iostream>
+#include <string>
+
+const int N = 3;
+const int M = 4;
+
+using namespace std;
+
+int main()
+{
+    int **arr = new int*[N];
+    for (int i = 0; i < N; i++) {
+        arr[i] = new int[M];
+    }
+    
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < M; j++) {
+            arr[i][j] = i*(j+1);
+        }
+    }
+    
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < M; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+    
+    for (int i = 0; i < N; i++) {
+        delete [] arr[i];
+    }
+    delete[] arr;   
+
+}
+/*
+0 0 0 0 
+1 2 3 4 
+2 4 6 8 
+*/
